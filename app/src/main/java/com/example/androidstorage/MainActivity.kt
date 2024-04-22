18e75663736449b041d46cace81e0f5c177d6ccd
KseniaSerko <kseniaserko@gmail.com>
2024-04-22 19:38:12 +0300

    Setting up internal storage recyclerView, taking photos and saving them, fixing some issues

--- app/src/main/java/com/example/androidstorage/MainActivity.kt
+++ app/src/main/java/com/example/androidstorage/MainActivity.kt
@@ -3,20 +3,72 @@ package com.example.androidstorage
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.androidstorage.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.IOException
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var internalStoragePhotoAdapter: InternalStoragePhotoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        internalStoragePhotoAdapter = InternalStoragePhotoAdapter {
+            val isDeletionSuccessful = deletePhotoFromInternalStorage(it.name)
+            if (isDeletionSuccessful) {
+                loadPhotosFromInternalStorageIntoRecyclerView()
+                Toast.makeText(this, "Photo successfully deleted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Failed to delete photo", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val takePhoto = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
+            val isPrivate = binding.switchPrivate.isChecked
+            if (isPrivate) {
+                val isSavedSuccessfully =
+                    it?.let { it1 -> savePhotoToInternalStorage(UUID.randomUUID().toString(), it1) }
+                if (isSavedSuccessfully == true) {
+                    loadPhotosFromInternalStorageIntoRecyclerView()
+                    Toast.makeText(this, "Photo saved successfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Failed to save photo", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.btnTakePhoto.setOnClickListener {
+            takePhoto.launch()
+        }
+
+        setupInternalStorageRecyclerView()
+        loadPhotosFromInternalStorageIntoRecyclerView()
+    }
+
+    private fun setupInternalStorageRecyclerView() = binding.rvPrivatePhotos.apply {
+        adapter = internalStoragePhotoAdapter
+        layoutManager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
+    }
+
+    private fun loadPhotosFromInternalStorageIntoRecyclerView() {
+        lifecycleScope.launch {
+            val photos = loadPhotoFromInternalStorage()
+            internalStoragePhotoAdapter.submitList(photos)
+        }
     }
 
     private fun deletePhotoFromInternalStorage(filename: String): Boolean {
