e84acbc0989993996cae07244d9d5097c4a3a10b
KseniaSerko <kseniaserko@gmail.com>
2024-04-22 18:10:36 +0300

    Saving photo to internal storage

---
+++ app/src/main/java/com/example/androidstorage/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.androidstorage
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidstorage.databinding.ActivityMainBinding
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun savePhotoToInternalStorage(filename: String, bmp: Bitmap): Boolean {
+        return try {
+            openFileOutput("$filename.jpg", MODE_PRIVATE).use { stream ->
+                if (!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)) {
+                    throw IOException("Couldn't save bitmap.")
+                }
+            }
+            true
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
+
+    }
+}
