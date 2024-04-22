722a49557d126d86c886ff278a9ab0f1086f9744
KseniaSerko <kseniaserko@gmail.com>
2024-04-22 19:08:05 +0300

    Loading photo from internal storage

--- app/src/main/java/com/example/androidstorage/MainActivity.kt
+++ app/src/main/java/com/example/androidstorage/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.androidstorage
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidstorage.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+
+    private suspend fun loadPhotoFromInternalStorage(): List<InternalStoragePhoto> {
+        return withContext(Dispatchers.IO) {
+            val files = filesDir.listFiles()
+            files?.filter { it.canRead() && it.isFile && it.name.endsWith(".jpg") }?.map {
+                val bytes = it.readBytes()
+                val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                InternalStoragePhoto(it.name, bmp)
+            } ?: listOf()
+        }
     }
 
     private fun savePhotoToInternalStorage(filename: String, bmp: Bitmap): Boolean {
